f26c0453ffda5b7b2a644aca41509bd65a8ea63a
Kola Antoshkiv <ak.pkqtor@gmail.com>
2020-12-23 15:42:01 +0200

    Modifide calc pro lv

--- index.js
+++ index.js
@@ -1,38 +1,44 @@
-const sum = function(a,b){
-  return a+b;
+const sum = function (a, b) {
+  return a + b;
 }
-const sub = function(a,b){
-  return a-b;
+const sub = function (a, b) {
+  return a - b;
 }
-const mul = function(a,b){
-  return a*b;
+const mul = function (a, b) {
+  return a * b;
 }
-const div = function(a,b){
-  return a/b;
+const div = function (a, b) {
+  return a / b;
 }
 
 /**  
  * функцыя кадькулятор
  * @returns {number | null}
 */
-const calc = function(){
+const calc = function () {
   const a = +prompt("Вв первое число");
   const b = +prompt("Вв второе число");
   const z = prompt("Вв знак");
-
-  switch(z){
-    case "+":{
-      return sum(a,b);
-    }case "-":{
-      return sub(a,b);
-    }case "*":{
-      return mul(a,b);
-    }case "/":{
-      return div(a,b);
-    }default:{
-      return null;
+  let f;
+  switch (z) {
+    case "+": {
+      f = sum;
+      break;
+    } case "-": {
+      f = sub;
+      break;
+    } case "*": {
+      f = mul;
+      break;
+    } case "/": {
+      f = div;
+      break;
     }
   }
+  if (typeof f === 'function') {
+    return f(a, b);
+  }
+  return null;
 }
 
 // console.log(calc());
@@ -42,14 +48,14 @@ const calc = function(){
  * @param {number}
  * @returns {string}
 */
-const decad = function(dayNumber){
-  if(dayNumber > 0 && dayNumber <= 10){
+const decad = function (dayNumber) {
+  if (dayNumber > 0 && dayNumber <= 10) {
     return "Число попадает в первую декаду";
-  }else if(dayNumber > 10 && dayNumber <= 20){
+  } else if (dayNumber > 10 && dayNumber <= 20) {
     return "Число попадает в вторую декаду";
-  }else if(dayNumber > 20 && dayNumber <= 31){
+  } else if (dayNumber > 20 && dayNumber <= 31) {
     return "Число попадает в третью декаду";
-  }else{
+  } else {
     return "ERROR";
   }
 }
