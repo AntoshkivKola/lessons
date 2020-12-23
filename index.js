4e1f0098f54b3c4b495389f44a4e33ad6d5dbcff
Kola Antoshkiv <ak.pkqtor@gmail.com>
2020-12-23 15:04:35 +0200

    Add simple js calc

--- index.js
+++ index.js
@@ -1,77 +1,24 @@
-const userInput1 = prompt("Введите число 1:");
-const userInput2 = prompt("Введите число 2:");
+/* функцыя кадькулятор
 
-const maxOfTwo = function(a, b){
-  const num1 = +a;
-  const num2 = +b;
-  if(!isNaN(num1) && !isNaN(num2) ){
-    if(num1 > num2){
-      return num1;
-    }else{
-      return num2;
-    }
-  }else{
-    alert("Вы ввели не числа!");
-  }
-}
+*/
 
-const minOfTwo = function(a, b){
-  const num1 = +a;
-  const num2 = +b;
-  if(!isNaN(num1) && !isNaN(num2) ){
-  
-    if(num1 < num2){
-      return num1;
-    }else{
-      return num2;
-    }
-  }else{
-    alert("Вы ввели не числа!");
-  }
-}
 
-const parity = function(a){
-  const num1 = +a;
-  if(!isNaN(num1)){
-    if( (num1%2) === 0){
-      return true;
-    }else{
-      return false;
+const calc = function(){
+  const a = +prompt("Вв первое число");
+  const b = +prompt("Вв второе число");
+  const z = prompt("Вв знак");
+
+  switch(z){
+    case "+":{
+      return a + b;
+    }case "-":{
+      return a - b;
+    }case "*":{
+      return a * b;
+    }case "/":{
+      return a / b;
     }
   }
 }
 
-
-
-let a = prompt("Введите числа  квадратного уравнения: a");
-let b = prompt("Введите числа  квадратного уравнения: b");
-let c = prompt("Введите числа  квадратного уравнения: c");
-const getExpresion = function(numA, numB, numC){
-  a = +numA;
-  b = +numB;
-  c = +numC;
-  
-  if(!isNaN(a) && !isNaN(b) && !isNaN(c) ){
-    const D = (b**2)-(4*a*c);
-    
-    if(D > 0){
-      const x1 = ( -b + D**0.5 ) / (2*a);
-      const x2 = ( -b - D**0.5 ) / (2*a);
-    
-    const result = [x1,x2];
-    
-    
-    return result;
-    }
-  }else{
-    console.log("error");
-  }
-}
-
-console.log("max: ",maxOfTwo(userInput1, userInput2));
-console.log("min: ",minOfTwo(userInput1, userInput2));
-console.log(parity(userInput1));
-
-
-let res = getExpresion(a, b, c);
-console.log("x1: ",res[0],"x2:", res[1]);
+console.log(calc());
