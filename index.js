9c914c14dee8bb543fff62ce33c4d8e16bcbaa0e
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-01 13:43:06 +0200

    Add createFamily() function hw

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
-function createMultiplicationtable() {
-  const table = {};
-
-  for (let i = 1; i <= 10; i++) {
-    for (let j = 1; j <= 10; j++) {
-      table[`${i} * ${j} = `] = i * j;
-    }
-  }
-
-  return table;
-}
-console.log(createMultiplicationtable());
-
-function cvartal(n) {
-  if (n > 12) {
+function createFamily() {
+  const countOfRelatives = +prompt('Введите кол-во вашых родствиников');
+  if (isNaN(countOfRelatives)) {
     return false;
   }
-  return Math.ceil(n/3);
+  const family = {}
+  let name = '';
+  let info = '';
+  for (let i = 0; i < countOfRelatives; i++) {
+    name = prompt(`Введите имя ${i+1} родствиника: `);
+    info = prompt(`Введите информацыю про ${i+1} родствиника: `);
+    family[name] = info;
+  }
+  return family;
 }
-console.log(cvartal(12));
+console.log(createFamily());
