2df2af461bebaacf0c21edd40a92f7cadd06bf4e
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-03 11:46:01 +0200

    Add pop and unshift method`s to MyArray

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ function MyArrayProto() {
     this[this.length] = item;
     return ++this.length;
   };
+  
+  this.pop = function () {
+    if (this.length === 0) { return; }
+    const lastItem = this[this.length - 1];
+    delete this[--this.length];
+    return lastItem;
+  };
+
+  this.unshift = function (item) {
+    for(let i = this.length; i > 0; i--){
+      this[i] = this[i-1];
+    }
+    this[0] = item;
+    return ++this.length;
+  };
 }
 
 // Объекты с данными
@@ -15,3 +30,9 @@ function MyArray() {
 MyArray.prototype = new MyArrayProto();
 
 const myArray = new MyArray();
+myArray.push(1);
+myArray.push(2);
+myArray.push(3);
+myArray.push(4);
+myArray.unshift(8);
+console.log(myArray);
