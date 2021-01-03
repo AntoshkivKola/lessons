4238734b90f28c5efe7b5cb3dd0e2af12b57b8e0
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-03 12:32:59 +0200

    Modifide push and unshift to infinity arguments

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 "use strict";
 // Объект с логикой
 function MyArrayProto() {
-  this.push = function push(item) {
-    this[this.length] = item;
-    return ++this.length;
+  this.push = function push() {
+    for (let i = 0; i < arguments.length; i++) {
+      this[this.length] = arguments[i];
+      ++this.length;
+    }
+    return this.length;
   };
-  
+
   this.pop = function () {
     if (this.length === 0) { return; }
     const lastItem = this[this.length - 1];
@@ -13,12 +16,15 @@ function MyArrayProto() {
     return lastItem;
   };
 
-  this.unshift = function (item) {
-    for(let i = this.length; i > 0; i--){
-      this[i] = this[i-1];
+  this.unshift = function () {
+    for (let i = (this.length + arguments.length - 1); i > 0; i--) {
+      this[i] = this[i - (arguments.length)];
     }
-    this[0] = item;
-    return ++this.length;
+    for (let i = 0; i < arguments.length; i++) {
+      this[i] = arguments[i];
+      ++this.length;
+    }
+    return this.length;
   };
 }
 
@@ -34,5 +40,5 @@ myArray.push(1);
 myArray.push(2);
 myArray.push(3);
 myArray.push(4);
-myArray.unshift(8);
+myArray.unshift(7,8,9);
 console.log(myArray);
