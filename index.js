e5aaba8a0ac59567d74917d0519200a95ed19405
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-06 20:02:46 +0200

    Add logRange

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 'use strict';
 
-const numbers = [1, 2, 21, 54, 84, 7];
-const numbers2 = [1, 5, 7, 8];
-
-const resNumbers = [...numbers, ...numbers2];//spread
-console.log(resNumbers);
-
-const sum = (...args) => {//rest
-  let accumulator = null;
-  args.forEach((currentArg) => {
-    accumulator += currentArg;
-  });
-  return accumulator;
-}
-
-const sumReduse = (...rest) => {
-  return rest.reduce((accumulator, value) => {
-    return accumulator + value;
-  });
+const logRange = (min, max) => {
+  if(min > max){
+    return false;
+  }
+  console.log(min);
+  if (min < max) {
+    logRange(min + 1, max)
+  }
+  return true;
 }
