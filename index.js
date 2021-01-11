fb04883981baf28b4a1f398efb1648faadc28bbf
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-11 20:01:13 +0200

    Add something

--- index.js
+++ index.js
@@ -1,12 +1,42 @@
 'use strict';
 
-const logRange = (min, max) => {
-  if(min > max){
+const fact = (num) => {
+  if (num < 0) {
     return false;
   }
-  console.log(min);
-  if (min < max) {
-    logRange(min + 1, max)
+  if (num === 0 || num === 1) {
+    return 1;
   }
-  return true;
+  return num * fact(num - 1);
 }
+
+const powwer = (num, exp) => {
+  let pos = true;
+  if (exp < 0) {
+    exp = -exp;
+    pos = false;
+  }
+
+  if (exp === 1) {
+    return num;
+  }
+  if (exp === 0) {
+    return 1;
+  }
+  debugger;
+  const res = num * powwer(num, exp - 1);
+
+  if (pos) {
+    return res
+  }
+  return 1 / res;
+
+}
+
+const par = (num) => {
+  if(num === 1){
+    return '()'
+  }
+  return `(${par(num - 1 )})`;
+};
+console.log(par(5));
