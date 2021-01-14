30ce43d1754218a5b2779d6aebec2409c22a2226
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-14 12:06:55 +0200

    Add squirrels

--- index.js
+++ index.js
@@ -1,27 +1,46 @@
 'use strict';
 
-class User{
-  constructor(name, surName, age, isBanned = false){
+class Squirrel{
+  constructor(name, color){
     this.name = name;
-    this.surName = surName;
-    this.age = age;
-    this.isBanned = isBanned;
+    this.color = color;
   }
-
-  getFullName(){
-    return `${this.name} ${this.surName}`;
+  eat(){
+    return `${this.name} is eating`;
+  }
+  climb(){
+    return `${this.name} is climbing`;
   }
 }
 
-class Admin extends User{
-  constructor(name, surName, age){
-    super(name, surName, age, false)
+class FlyingSquirrel extends Squirrel{
+  constructor(name, color, maxDistance){
+    super(name, color);
+    this.maxDistance = maxDistance;
   }
-  ban(user){
-    user.isBanned = !user.isBanned;
-  }
-  
-}
 
-const u = new User('Leopold', 'Vladimirovich', 45);
-const admin = new Admin('Admin', 'Adminovich',15);
+  fly(){
+    // if(distance > this.maxDistance){
+    //   throw new RangeError('You can`t fly further than max distance');
+    // }
+
+    return `${this.name} is flying in ${Math.ceil( Math.random()* this.maxDistance)} metires`
+  }
+}
+const flySquirrel = new FlyingSquirrel("Leton", "orange", 130);
+
+class FableSquirrel extends FlyingSquirrel{
+  constructor(name, color, maxDistance, repertuar){
+    super(name, color, maxDistance);
+    this.repertuar = repertuar;
+  }
+  sing(){
+    this.repertuar.forEach(song => {
+      console.log(`${this.name} is sing for you '${song}' la-la-lala`);
+    });
+  }
+  dance(){
+    return `${this.name} is densing`
+  }
+}
+const fableSquirrel = new FableSquirrel('Fable', 'white', 60, ['smells like ten spiret', 'Save me', 'Dence with the divil']);
