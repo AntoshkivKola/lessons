41d5704d2cd4970f6ad3e6ec83b58b3f8af2134f
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-14 12:48:47 +0200

    Add pokimorphizm and incapsulation

--- index.js
+++ index.js
@@ -1,46 +1,27 @@
 'use strict';
 
-class Squirrel{
-  constructor(name, color){
+class Figure {
+  constructor(name) {
     this.name = name;
-    this.color = color;
   }
-  eat(){
-    return `${this.name} is eating`;
+  getArea() { }
+}
+
+class Circle extends Figure {
+  constructor(radius) {
+    super("Circle");
+    this._radius = radius;
   }
-  climb(){
-    return `${this.name} is climbing`;
+  getArea() {
+    return  Math.PI * this._radius * this._radius;
   }
 }
 
-class FlyingSquirrel extends Squirrel{
-  constructor(name, color, maxDistance){
-    super(name, color);
-    this.maxDistance = maxDistance;
+function getFigureArea(figure){
+  if(figure instanceof Figure){
+    return figure.getArea();
   }
-
-  fly(){
-    // if(distance > this.maxDistance){
-    //   throw new RangeError('You can`t fly further than max distance');
-    // }
-
-    return `${this.name} is flying in ${Math.ceil( Math.random()* this.maxDistance)} metires`
-  }
+  throw new TypeError();
 }
-const flySquirrel = new FlyingSquirrel("Leton", "orange", 130);
 
-class FableSquirrel extends FlyingSquirrel{
-  constructor(name, color, maxDistance, repertuar){
-    super(name, color, maxDistance);
-    this.repertuar = repertuar;
-  }
-  sing(){
-    this.repertuar.forEach(song => {
-      console.log(`${this.name} is sing for you '${song}' la-la-lala`);
-    });
-  }
-  dance(){
-    return `${this.name} is densing`
-  }
-}
-const fableSquirrel = new FableSquirrel('Fable', 'white', 60, ['smells like ten spiret', 'Save me', 'Dence with the divil']);
+const c = new Circle(5);
