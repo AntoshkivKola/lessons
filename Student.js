82d535cc242e13383282dddba9bc09059f117c57
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-15 19:45:16 +0200

    Refact again getCourse...

--- Student.js
+++ Student.js
@@ -24,11 +24,10 @@ class Student extends User {
   get year() {
     return this._year;
   }
-  getCourse(currentDate) {
-    const validDate =
-      (currentDate.getFullYear() === this.year.getFullYear() + 5 && currentDate.getMonth() <= 5 && currentDate.getDate() <= 30) ||
-      ((currentDate.getFullYear() < this.year.getFullYear() + 5) &&  (currentDate.getFullYear() > this.year.getFullYear())) ||
-      (currentDate.getFullYear() === this.year.getFullYear() && currentDate.getMonth() <= this.year.getMonth() && currentDate.getDate() <= this.year.getDate());
+  getCourse() {
+    const currentDate = new Date();
+    const validDate = (currentDate.getFullYear() <= this.year.getFullYear() + 5) &&  (currentDate.getFullYear() >= this.year.getFullYear());
+      
     if (typeof (currentDate) !== 'object') {
       throw new TypeError('the argument must be a date');
     }
@@ -48,9 +47,11 @@ class Student extends User {
   }
 }
 
-let currentDate = new Date(2021, 4, 15);
+//let currentDate = new Date(2021, 4, 15);
 const s = new Student('Tets', 'Testovich', new Date(2018, 8, 1));
-console.log(s.getCourse(currentDate)) // 4
-console.log(s.getCourse(2020, 4,6)) // TypeError('the argument must be a date');
-let currentDate2 = new Date(2028, 4, 15);
-console.log(s.getCourse(currentDate2)) // RangeError('Range EROR');
+console.log(s.getCourse()); // 4
+const s1 = new Student('1','1', new Date(2021,0,1));
+console.log(s1.getCourse()) // 1
+// console.log(s.getCourse(2020, 4,6)) // TypeError('the argument must be a date');
+// let currentDate2 = new Date(2028, 4, 15);
+// console.log(s.getCourse(currentDate2)) // RangeError('Range EROR');
