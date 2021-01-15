ed918328c8d14b41b068c4ca57bd1a46c2c3d048
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-15 19:25:50 +0200

    Refact year in date

--- Student.js
+++ Student.js
@@ -15,7 +15,7 @@ class Student extends User {
    * 
    * @param {string} name 
    * @param {string} surName 
-   * @param {number} year 
+   * @param {Date} year 
    */
   constructor(name, surName, year) {
     super(name, surName);
@@ -25,20 +25,31 @@ class Student extends User {
     return this._year;
   }
   getCourse(currentDate) {
-    if (typeof (currentDate) !== "number") {
+    const validDate =
+      (currentDate.getFullYear() === this.year.getFullYear() + 5 && currentDate.getMonth() <= 5 && currentDate.getDate() <= 30) ||
+      ((currentDate.getFullYear() < this.year.getFullYear() + 5) &&  (currentDate.getFullYear() > this.year.getFullYear())) ||
+      (currentDate.getFullYear() === this.year.getFullYear() && currentDate.getMonth() <= this.year.getMonth() && currentDate.getDate() <= this.year.getDate());
+    if (typeof (currentDate) !== 'object') {
       throw new TypeError('the argument must be a date');
     }
-    if (currentDate > this.year + 4) {
-      throw new RangeError('no longer a student');
+    if (!validDate) {
+      throw new RangeError('Range EROR');
     }
-    if (currentDate < this.year) {
-      throw new RangeError('not a student yet');
+    // if (currentDate > this.year + 4) {
+    //   throw new RangeError('no longer a student');
+    // }
+    // if (currentDate < this.year) {
+    //   throw new RangeError('not a student yet');
+    // }
+    if(currentDate.getFullYear() === this.year.getFullYear() + 5){
+      return currentDate.getFullYear() - this.year.getFullYear();
     }
-    return currentDate - this.year + 1
+    return currentDate.getFullYear() - this.year.getFullYear() + 1;
   }
 }
 
-const s = new Student('Tets', 'Testovich', 2018);
-console.log(s.getCourse(2000)) // RangeError('not a student yet');
-console.log(s.getCourse(2030)) // RangeError('no longer a student');
-console.log(s.getCourse(2020)) // 3
+let currentDate = new Date(2021, 4, 15)
+const s = new Student('Tets', 'Testovich', new Date(2018, 8, 1));
+console.log(s.getCourse(currentDate)) // RangeError('not a student yet');
+console.log(s.getCourse(currentDate)) // RangeError('no longer a student');
+console.log(s.getCourse(currentDate)) // 3
--- constants.js
+++ constants.js
@@ -1,3 +1,4 @@
 const ADULT_AGE = 18;
 const Min_ZP = 4200;
 const MAX_WEIGHT_PASSENGER = 150;
+// (currentDate.getFullYear() === 2023 && currentDate.getMonth() <= 5 && currentDate.getDate() <= 30) || ((currentDate.getFullYear() < 2023 ) || (currentDate.getFullYear() > this.year.getFullYear()) || (currentDate.getFullYear()  === this.year.getFullYear() && currentDate.getMonth() <= this.year.getMonth() && currentDate.getDate() <= this.year.getDate()));
