b69996d77dcb6607a90f12045002d05a1fc81388
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-19 13:50:04 +0200

    Add checkSequence resolution on stack

--- index.js
+++ index.js
@@ -42,44 +42,32 @@ class Stack {
 
 const newStack = new Stack(5, 12, 13, 14, 15);
 
-const checkOfSequence = (symbol, index, arr) => {
-  const open = ['(', '{', '[', '<', '0'];
-  const close = [')', '}', ']', '>', '1'];
-  const res = close[open.indexOf(symbol)];
-
-  for (let i = index; i < arr.length; i++) {
-    if (open.includes(arr[i])) {
-      i = checkOfSequence(arr[i], i + 1, arr);
-      if (i === false) {
-        return false;
-      }
-      if (open.includes(arr[i])) {
-        i = checkOfSequence(arr[i], i + 1, arr);
-        if (i === false) {
-          return false;
-        }
-      }
-    }
-
-    if (arr[i] === res) {
-      return i + 1;
-    }
-
-    if (close.includes(arr[i])) {
-      return false;
-    }
-
-  }
-  return false;
-}
 
 const checkSequence = (str) => {
-  const arr = str.split('');
-  let id = checkOfSequence(arr[0], 1, arr);
-  if (id < arr.length) {
-    if (checkOfSequence(arr[id], id + 1, arr) !== arr.length) {
-      return false
+  const openStack = new Stack();
+  const closeStack = new Stack();
+  const open = ['(', '{', '[', '<', '0'];
+  const close = [')', '}', ']', '>', '1'];
+  
+ 
+  for (symbol of str) {
+    if (open.includes(symbol)) {
+
+      openStack.push(symbol);
+      closeStack.push(close[open.indexOf(symbol)]);
     }
+    if (openStack.isEmpty) {
+      return false;
+    }
+    if (close.includes(symbol)) {
+      if(closeStack.pick() !== symbol){
+        return false;
+      }
+      openStack.pop();
+      closeStack.pop();
+    }
+
   }
-  return true;
+
+  return openStack.isEmpty;
 };
