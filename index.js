dab3d1a9cc2c2086b48e70049364e86ffff86f7a
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-19 19:43:50 +0200

    Add Queue

--- index.js
+++ index.js
@@ -1,73 +1,42 @@
-class Stack {
-  constructor(maxSize = 10, ...array) {
-    this._maxSize = maxSize;
-    this._size = 0;
-
-    for (const item of array) {
-      this.push(item);
-    }
-  }
-
-  get isEmpty() {
-    return this._size === 0;
+class Queue {
+  constructor(...args) {
+    this._head = 0;
+    this._tail = 0;
+    args.forEach((elem) => {
+      this.push(elem);
+    });
   }
 
   get size() {
-    return this._size;
+    return this._tail - this._head;
   }
 
   push(value) {
-    if (this.size >= this._maxSize) {
-      throw new RangeError('Stack overflow');
-    }
-    this[`_${this.size}`] = value;
-    this._size++;
+    this[this._tail] = value;
+    this._tail++;
     return this.size;
   }
 
   pop() {
-    if (this.size <= 0) {
-      return;
+    if (this.size) {
+      const lastItem = this[this._head];
+      delete this[this._head++];
+      return lastItem;
     }
-    const lastItem = this[`_${this.size - 1}`];
-    delete this[`_${this.size - 1}`];
-    this._size--;
-    return lastItem;
   }
 
-  pick() {
-    return this[`_${this.size - 1}`];
+  mergeQueue(q1, q2) {
+    const q = new Queue();
+    while(q1.size) {
+      q.push(q1.pop())
+    }
+    while(q2.size) {
+      q.push(q2.pop())
+    }
+    return q;
   }
 }
-
-const newStack = new Stack(5, 12, 13, 14, 15);
-
-
-const checkSequence = (str) => {
-  const openStack = new Stack();
-  const closeStack = new Stack();
-  const open = ['(', '{', '[', '<', '0'];
-  const close = [')', '}', ']', '>', '1'];
-  
- 
-  for (symbol of str) {
-    if (open.includes(symbol)) {
-
-      openStack.push(symbol);
-      closeStack.push(close[open.indexOf(symbol)]);
-    }
-    if (openStack.isEmpty) {
-      return false;
-    }
-    if (close.includes(symbol)) {
-      if(closeStack.pick() !== symbol){
-        return false;
-      }
-      openStack.pop();
-      closeStack.pop();
-    }
-
-  }
-
-  return openStack.isEmpty;
-};
+const q = new Queue();
+const q1 = new Queue(1, 2, 3);
+const q2 = new Queue(1, 8, 4);
+const q3 = q.mergeQueue(q1, q2);
