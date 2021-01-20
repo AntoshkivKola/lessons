0c2fdeced6bb9333628349614baf6d6e1ac378c8
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-01-20 19:58:19 +0200

    Add Set example

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-const user1 = {
-  id: 1,
-  name: "John",
-  age: 18,
-};
-
-const user2 = {
-  id: 2,
-  name: 'Jane',
-  age: 24,
-};
-
-const johnMsgs = ['dasda', 'dasda', '132'];
-const janeMsgs = ['fafeaw', 'dar3rsacasda', '1dawdaderfthd32'];
-
-const userMsgs = new Map();
-userMsgs.set(user1.id, johnMsgs);
-userMsgs.set(user2.id, janeMsgs);
-
-const getUserMsgs = (userId) => {
-  if (userMsgs.has(userId)) {
-    return userMsgs.get(userId)
-  }
-  throw new Error('not existing user!');
-}
+const nums1 = [1,5,3,6,1,1,1,2,5,6,6,1,5];
+const nums2 = [8,7,1,2,3,6,8,4,9,5,8];
+const unique = [...new Set([...nums1,...nums2])].sort();
