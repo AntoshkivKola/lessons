ac663a7e7c0c8769609169fe65ce6fe858968d18
Kola Antoshkiv <ak.pkqtor@gmail.com>
2021-02-03 19:49:29 +0200

    Add loadImage

--- sanbox.js
+++ sanbox.js
@@ -1,8 +1,27 @@
 'use strict';
-let d = 0;
-const p = fetch('./constants.js')
-  .then((response) => response.text())
-  .then((data) => { d = data; console.log(data) })
-  .catch((err)=>{
-    console.log('common error!!');
+
+function loadImage(src) {
+  const img = new Image() // document .createElement('img)
+
+  img.setAttribute('src', src);
+  return new Promise((resolve, reject) => {
+    img.addEventListener('load', () => {
+      resolve(img);
+    })
+
+    img.addEventListener('error', () => {
+      reject(new Error('Img has not been delivered'));
+    })
   });
+}
+
+loadImage('https://i.pinimg.com/originals/3b/8a/d2/3b8ad2c7b1be2caf24321c852103598a.jpg')
+  .then((img) => {
+    document.body.append(img);
+  })
+  .catch((error)=>{
+    alert(error);
+  })
+
+
+
